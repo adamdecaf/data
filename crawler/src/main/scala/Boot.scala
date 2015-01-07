bfe77ab60aed813c1d295eb46399a41bfc0c10e6
Adam Shannon <adam.shannon@banno.com>
2015-01-07 14:41:08 -0600

    adding notes to myself

--- crawler/src/main/scala/Boot.scala
+++ crawler/src/main/scala/Boot.scala
@@ -7,3 +7,18 @@ object Boot extends App
   runMigrations()
 
 }
+
+// == crawler ==
+// table of "crawlable conical urls"
+// - needs a seed list to start with, google news, archive.org
+// join ^ with recently crawled sites to generate list of "next crawls"
+// send ^ off to be crawled within this app
+// - needs rate limiting per host
+// todo: some distributed lock? reserve in psql table?
+// on response:
+// - encrypt and store in s3, postgres
+// - send html ids from ^ to extractor
+
+// == extractor ==
+// pull out urls from html, make conical urls and insert new ones into "to crawl" table
+// - ^ urls need a last_crawled_at which determines when to crawl again
